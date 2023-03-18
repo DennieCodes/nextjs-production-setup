bc9e958e8e65cafc4dacd2765e62a4966d79a5cc
Dennie <oculareo@gmail.com>
2023-03-18 04:17:36 -0400

    Added Jest coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,25 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 // eslint-disable-next-line no-undef
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: ['./setup-jest.ts'],
-	transform: {
-		// '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-		// '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-		'^.+\\.tsx?$': [
-			'ts-jest',
-			{
-				tsconfig: 'tsconfig.jest.json',
-			},
-		],
-	},
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["./setup-jest.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.jest.json",
+      },
+    ],
+  },
 };
